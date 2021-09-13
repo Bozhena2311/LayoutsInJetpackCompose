ea15510f05e60cfecef9749af7c0fc872e9ab3d7
Bozhena Balzhanova <bozhena.balzhanova@gmail.com>
2021-09-13 15:07:32 +0300

    end part 6 "Working with list", 7 "Create your custom layout", 8 "Complex custom layout"

--- app/src/main/java/com/example/layoutsinjetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/layoutsinjetpackcompose/MainActivity.kt
@@ -1,30 +1,25 @@
 package com.example.layoutsinjetpackcompose
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.AccountBox
-import androidx.compose.material.icons.filled.Favorite
-import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.AlignmentLine
+import androidx.compose.ui.layout.FirstBaseline
+import androidx.compose.ui.layout.Layout
+import androidx.compose.ui.layout.layout
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
-import coil.compose.rememberImagePainter
 import com.example.layoutsinjetpackcompose.ui.theme.LayoutsInJetpackComposeTheme
+import kotlin.math.max
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -33,24 +28,201 @@ class MainActivity : AppCompatActivity() {
             LayoutsInJetpackComposeTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    ImageList()
+                    BodyContent()
                 }
             }
         }
     }
 }
 
-@Composable
-fun ImageList() {
-    //сохраняем состояние скроллинга
-    val scrollState = rememberLazyListState()
+fun Modifier.firstBaselineToTop(
+    firstBaselineToTop: Dp
+) = this.then(
+    layout { measurable, constraints ->
+        val placeable = measurable.measure(constraints)
 
-    LazyColumn(state = scrollState) {
-        items(100) {
-            ImageListItem(it)
+        //Проверка composable элемента на первую базовую линию
+        check(placeable[FirstBaseline] != AlignmentLine.Unspecified)
+        val firstBaseline = placeable[FirstBaseline]
+
+        //Высота composable элемента с paddingami - первая базовая линия
+        val placeableY = firstBaselineToTop.roundToPx() - firstBaseline
+        val height = placeable.height + placeableY
+        layout(placeable.width, height) {
+            //здесь будет размещено composable
+            placeable.placeRelative(0, placeableY)
+        }
+    }
+)
+
+
+@Composable
+fun StaggeredGrid(
+    modifier: Modifier = Modifier,
+    rows: Int = 3,
+    content: @Composable () -> Unit
+) {
+    Layout(
+        modifier = modifier,
+        content = content
+    ) { measurables, constraints ->
+
+        val rowWidths = IntArray(rows) { 0 }
+
+        val rowHeights = IntArray(rows) { 0 }
+
+        val placeables = measurables.mapIndexed { index, measurable ->
+
+            val placeable = measurable.measure(constraints)
+
+            val row = index % rows
+
+            rowWidths[row] += placeable.width
+            rowHeights[row] = max(rowHeights[row], placeable.height)
+
+            placeable
+        }
+
+        val width = rowWidths.maxOrNull()
+            ?.coerceIn(constraints.minWidth.rangeTo(constraints.maxWidth)) ?: constraints.minWidth
+
+        val height = rowHeights.sumOf { it }
+            .coerceIn(constraints.minHeight.rangeTo(constraints.maxHeight))
+
+        val rowY = IntArray(rows) { 0 }
+        for (i in 1 until rows) {
+            rowY[i] = rowY[i-1] + rowHeights[i-1]
+        }
+
+        layout(width, height) {
+
+            val rowX = IntArray(rows) { 0 }
+
+            placeables.forEachIndexed { index, placeable ->
+                val row = index % rows
+                placeable.placeRelative(
+                    x = rowX[row],
+                    y = rowY[row]
+                )
+                rowX[row] += placeable.width
+            }
         }
     }
 }
+
+@Composable
+fun Chip(modifier: Modifier = Modifier, text: String) {
+    Card(
+        modifier = modifier,
+        border = BorderStroke(color = Color.Black, width = Dp.Hairline),
+        shape = RoundedCornerShape(8.dp)
+    ) {
+        Row(
+            modifier = Modifier.padding(start = 8.dp, top = 4.dp, end = 8.dp, bottom = 4.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Box(
+                modifier = Modifier
+                    .size(16.dp, 16.dp)
+                    .background(color = MaterialTheme.colors.secondary)
+            )
+            Spacer(Modifier.width(4.dp))
+            Text(text = text)
+        }
+    }
+}
+
+val topics = listOf(
+    "Arts & Crafts", "Beauty", "Books", "Business", "Comics", "Culinary",
+    "Design", "Fashion", "Film", "History", "Maths", "Music", "People", "Philosophy",
+    "Religion", "Social sciences", "Technology", "TV", "Writing"
+)
+
+@Composable
+fun BodyContent(modifier: Modifier = Modifier) {
+    Row(modifier = modifier.horizontalScroll(rememberScrollState())) {
+        StaggeredGrid {
+            for (topic in topics) {
+                Chip(modifier = Modifier.padding(8.dp), text = topic)
+            }
+        }
+    }
+}
+
+
+@Preview
+@Composable
+fun BodyContentPreview() {
+    LayoutsInJetpackComposeTheme {
+        BodyContent()
+    }
+}
+/*@Preview
+@Composable
+fun ChipPreview() {
+    LayoutsInJetpackComposeTheme {
+        Chip(text = "Hi there")
+    }
+}*/
+
+/*
+@Composable
+fun BodyContent(modifier: Modifier = Modifier) {
+    MyOwnColumn(modifier.padding(8.dp)) {
+        Text("MyOwnColumn")
+        Text("places items")
+        Text("vertically")
+        Text("We've done it by hand!")
+    }
+}
+
+/*@Composable
+fun CustomLayout(
+    modifier: Modifier = Modifier,
+    // атрибуты собственной разметки
+    content: @Composable () -> Unit
+) {
+    Layout(
+        modifier = modifier,
+        content = content
+    ) { measurables, constraints ->
+        //измерьте и расположите здесь дочерние элементы, с учетом логики органичений здесь
+    }
+}*/
+
+@Composable
+fun MyOwnColumn(
+    modifier: Modifier = Modifier,
+    content: @Composable () -> Unit
+) {
+    Layout(
+        modifier = modifier,
+        content = content
+    ) { measurables, constraints ->
+        //не ограничиваейте дочерние элементы дальше, измеряйте их с учетом заданных constraint
+        //список измеренных дочерних элементов
+        val placeables = measurables.map { measurable ->
+        //Измерьте каждый дочерний элемент
+        measurable.measure(constraints)
+        }
+        //Отслеживайте координаты где мы разместили дочерние элементы
+        var yPosition = 0
+        //Установите размер layouta как можно больше
+        layout(constraints.maxWidth, constraints.maxHeight) {
+            //Поместите детей в родительский макет
+            placeables.forEach {placeable ->
+            //Расположите элементы на экране
+                placeable.placeRelative(x = 0, y = yPosition)
+
+                //Запишите y координаты
+                yPosition += placeable.height
+            }
+        }
+
+    }
+}
+
+
 
 @Composable
 fun ImageListItem(index: Int) {
@@ -65,6 +237,42 @@ fun ImageListItem(index: Int) {
         )
         Spacer(Modifier.width(10.dp))
         Text("Item #$index", style = MaterialTheme.typography.subtitle1)
+    }
+}
+
+@Composable
+fun ScrollingList() {
+    //количество элементов в списке
+    val listSize = 100
+    //сохраняем состояние скроллинга
+    val scrollState = rememberLazyListState()
+    //сохраняем область сопрограммы, в которой будет выполняться наш анимированный список
+    val coroutineScope = rememberCoroutineScope()
+
+    Row {
+        Button(onClick = {
+            coroutineScope.launch {
+                //0 - индекс первого элемента
+                scrollState.animateScrollToItem(0)
+            }
+        }) {
+            Text("Скролл вверх")
+        }
+
+        Button(onClick = {
+            coroutineScope.launch {
+                //размер списка - 1 = индекс последнего элемента списка
+                scrollState.animateScrollToItem(listSize - 1)
+            }
+        }) {
+            Text("Скролл вниз")
+        }
+    }
+
+    LazyColumn(state = scrollState) {
+        items(listSize) {
+            ImageListItem(it)
+        }
     }
 }
 
@@ -104,13 +312,13 @@ fun LayoutsCodelab() {
     }                                               //для повторного использования кода и тестируемости
 }
 
-@Composable
+/*@Composable
 fun BodyContent(modifier: Modifier = Modifier) {
     Column(modifier = Modifier) {
         Text(text = "Hi there!")
         Text(text = "Thanks for going through the Layouts codelab")
     }
-}
+}*/
 
 @Composable
 fun PhotographerCard(modifier: Modifier = Modifier) {
@@ -144,9 +352,33 @@ fun PhotographerCard(modifier: Modifier = Modifier) {
 
 @Preview
 @Composable
+fun BodyContent() {
+    LayoutsInJetpackComposeTheme {
+        BodyContent()
+    }
+}
+
+@Preview
+@Composable
+fun TextWithPaddingToBaselinePreview() {
+    LayoutsInJetpackComposeTheme {
+        Text("Hi there!", Modifier.firstBaselineToTop(32.dp))
+    }
+}
+
+@Preview
+@Composable
+fun TextWithNormalPaddingPreview() {
+    LayoutsInJetpackComposeTheme {
+        Text("Hi there!", Modifier.padding(top = 32.dp))
+    }
+}
+
+@Preview
+@Composable
 fun SimpleListPreview() {
     LayoutsInJetpackComposeTheme {
-        SimpleList()
+        ScrollingList()
     }
 }
 
@@ -164,4 +396,4 @@ fun PhotographerCardPreview() {
     LayoutsInJetpackComposeTheme {
         PhotographerCard()
     }
-}
+}*/
